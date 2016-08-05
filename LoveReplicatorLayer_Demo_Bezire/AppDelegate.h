beffd3e78952c4be39d098e7d294953f8b459531
sp55 <1172242456@qq.com>
2016-08-05 15:55:47 +0800

    心形动画和音乐进度条 贝塞尔曲线和CAReplicatorLayer
    心形动画和音乐进度条 贝塞尔曲线和CAReplicatorLayer

---
+++ LoveReplicatorLayer_Demo_Bezire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoveReplicatorLayer_Demo_Bezire
+//
+//  Created by admin on 16/8/5.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoveReplicatorLayer_Demo_Bezire/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoveReplicatorLayer_Demo_Bezire
+//
+//  Created by admin on 16/8/5.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
